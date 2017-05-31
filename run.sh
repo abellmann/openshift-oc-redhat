e7e22c91d156c2b5e472012b41fab6495030ca77
Andreas Bellmann <andreas.bellmann@gmail.com>
2017-05-31 14:49:05 +0200

    mount the Users directory at /c/Users to share data with the vm

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -v ~/.bash_history:/root/.bash_history openshift-oc-redhat /bin/bash
+docker run --rm -it -v ~/.bash_history:/root/.bash_history -v /c/Users:/c/Users openshift-oc-redhat /bin/bash
